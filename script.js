5d44f96fbcffd74caf3289dfa55a42fded484822
Patrick42702 <patrick42702@yahoo.com>
2023-09-05 16:48:13 -0400

    Initialized files
    Created all files, began graph creation

---
+++ script.js
@@ -0,0 +1,14 @@
+function createGraph(size) {
+  const graphContainer = document.createElement("div");
+  graphContainer.classList.add("graph-container");
+  document.body.appendChild(graphContainer);
+
+  for (let i = size; i < size; i++){
+    
+    for (let j = size; j < size; j++){
+
+    }
+  }
+}
+
+createGraph();
