b0cdd6f91b1b45b2be677a13eda6c51cdcd1c123
Patrick42702 <patrick42702@yahoo.com>
2023-09-08 21:49:54 -0400

    Added form and button
    Added form and button to change the size of the grid, or erase

--- script.js
+++ script.js
@@ -7,12 +7,44 @@ function scaleGrid(size) {
   return `height: ${squareHeight}px; width: ${squareWidth}px;`;
 }
 
+function createMenu(graphContainer) {
+  const div = document.createElement("div");
+  const button = document.createElement("button");
+  const form = document.createElement("form");
+
+  div.classList.add("menu");
+
+  button.classList.add("erase");
+  button.textContent = "ERASE";
+
+  form.classList.add("form");
+  const label = document.createElement("label");
+  label.textContent = "Enter number here:";
+  form.appendChild(label);
+  const input = document.createElement("input");
+  input.type = "number";
+  input.id = "userNumber";
+  form.appendChild(input);
+
+  const submitButton = document.createElement("button");
+  submitButton.setAttribute("type", "submit");
+  submitButton.textContent = "Submit";
+  submitButton.classList.add("submit");
+  form.appendChild(submitButton);
+
+  div.appendChild(button);
+  div.appendChild(form);
+  graphContainer.appendChild(div);
+}
+
 function createGraph(size) {
   const container = document.querySelector(".layout");
 
   const graphContainer = document.createElement("div");
   graphContainer.classList.add("graph-container");
   container.appendChild(graphContainer);
+
+  createMenu(graphContainer);
 
   for (let i = 0; i < size; i++) {
     const rowContainer = document.createElement("div");
@@ -32,16 +64,50 @@ function createGraph(size) {
   square.forEach((box) => (box.style.cssText = squareProperties));
 }
 
-createGraph(25);
-
-const square = document.querySelectorAll(".square");
+createGraph(10);
 
 function handleHover(box) {
-  box.style['background-color'] = "gray";
+  box.style["background-color"] = "gray";
 }
 
-square.forEach((box) => {
-  box.addEventListener("mouseenter", () => {
-    handleHover(box);
+function handleErase() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => (square.style["background-color"] = "white"));
+}
+
+function handleNumber() {
+  console.log("handle number called.");
+  const input = document.querySelector("input");
+  let size = input.value;
+  createGraph(size);
+}
+
+function initializeEvents() {
+  const form = document.querySelector(".form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("handle number called.");
+    const input = document.querySelector("input");
+    let size = input.value;
+
+    const layout = document.querySelector(".layout");
+    const graphContainer = document.querySelector(".graph-container");
+    layout.removeChild(graphContainer);
+
+    createGraph(size);
+    initializeEvents();
   });
-});
+
+  const square = document.querySelectorAll(".square");
+
+  square.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      handleHover(box);
+    });
+  });
+
+  const erase = document.querySelector(".erase");
+  erase.addEventListener('click', handleErase);
+}
+
+initializeEvents();
